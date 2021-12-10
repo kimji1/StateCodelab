035bcccf4bc766b6804310c0ec05db405dfe7c9e
kimji1 <dev.kimji1@gmail.com>
2021-12-10 21:37:28 +0900

    6. State in Compose

--- start/src/main/java/com/codelabs/state/todo/TodoComponents.kt
+++ start/src/main/java/com/codelabs/state/todo/TodoComponents.kt
@@ -49,6 +49,7 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
